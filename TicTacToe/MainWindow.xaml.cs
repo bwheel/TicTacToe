04ef9293877b728c238b0923492eddbbcccde225
Byron Wheeler <1660767+bwheel@users.noreply.github.com>
2018-11-29 10:40:11 -0600

    fix spelling

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TickTackToe
+namespace TicTacToe
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
--- TicTacToe/Properties/Resources.Designer.cs
+++ TicTacToe/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TickTackToe.Properties
+namespace TicTacToe.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace TickTackToe.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TickTackToe.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TicTacToe.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
